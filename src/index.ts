93fcdec8c38a83460c76c7592d265a8e522736f3
Oleksandr Sukhinin <syhinin@gmail.com>
2026-04-09 09:17:51 +0200

    implement sequrity middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import subjectsRouter from "./routes/subjects";
+import securityMiddleware from "./middleware/security";
 
 const app = express();
 const PORT = 8000;
@@ -10,14 +11,19 @@ if (!process.env.FRONTEND_URL) {
   throw new Error("FRONTEND_URL environment variable is required");
 }
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
-app.use('/api/v1/subjects', subjectsRouter);
+
+app.use(securityMiddleware)
+
+app.use("/api/v1/subjects", subjectsRouter);
 
 app.get("/", (_req, res) => {
   res.json({ message: "Classroom API is running" });
